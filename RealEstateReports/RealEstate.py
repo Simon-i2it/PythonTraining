55e4fe099adca6a983e79a1175c09e65b28c5393
Simon Alphonse <IAmSimon@outlook.in>
2023-04-22 08:45:56 +0530

    Added Style to table

--- RealEstateReports/RealEstate.py
+++ RealEstateReports/RealEstate.py
@@ -3,10 +3,10 @@ import pandas
 
 from enum import Enum
 from pandas import DataFrame
-from reportlab.pdfgen import canvas
 from reportlab.lib import colors
+from reportlab.pdfgen import canvas
+from reportlab.platypus import Table, TableStyle
 from reportlab.lib.pagesizes import landscape, A4
-from reportlab.platypus import SimpleDocTemplate, Table
 
 
 class Column(Enum):
@@ -60,11 +60,24 @@ class RealEstate:
         pdf_row_px = 20
         pdf_margin_px = 80
 
-        data_frame = data_frame.reset_index()
         pdf_file = canvas.Canvas(f"RealEstateReports/{title}.pdf", pagesize, bottomup=100)
+        pdf_file.drawString(
+            pdf_margin_px, pagesize[1] - pdf_margin_px + 30, f"{title} :-"
+        )
+
+        data_frame = data_frame.reset_index()
         data = [list(data_frame)] + data_frame.values.tolist()
         
         table = Table(data)
+        table_style = TableStyle([
+            ('BACKGROUND', (0, 0), (-1, 0), colors.HexColor('#DCE6F1')), # Header row
+            ('TEXTCOLOR', (0, 0), (-1, 0), colors.HexColor('#333333')), # Header text color
+            ('BACKGROUND', (0, 1), (-1, -1), colors.HexColor('#F7FBFF')), # Data rows
+            ('TEXTCOLOR', (0, 1), (-1, -1), colors.HexColor('#333333')), # Data text color
+            ('GRID', (0, 0), (-1, -1), 1, colors.HexColor('#E2E2E2')) # Table grid
+            ])
+        table.setStyle(table_style)
+
         table.wrapOn(pdf_file, 0, 0)
         table.drawOn(
             pdf_file,
@@ -72,9 +85,6 @@ class RealEstate:
             pagesize[1] - pdf_row_px * data_frame.shape[0] - pdf_margin_px,
         )
 
-        pdf_file.drawString(
-            pdf_margin_px, pagesize[1] - pdf_margin_px + 30, f"{title} :-"
-        )
         pdf_file.save()
 
     # Execution
@@ -95,7 +105,7 @@ class RealEstate:
     )
 
     export_dataframe_to_pdf(
-        "3. Property's average Price, Square Footage, Number of Bed and Bath rooms in each property type and location",
+        "3. Average Price, Square Footage, Number of Bed and Bath rooms in each property type and location",
         get_summary_by_location_property_type(data_frame),
         landscape(A4),
     )
