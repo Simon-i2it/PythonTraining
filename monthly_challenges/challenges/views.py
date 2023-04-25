1600f68d20ea2c955fd05e186af4cec8f78f0fd3
Simon Alphonse <IAmSimon@outlook.in>
2023-04-25 13:01:02 +0530

    Added generic path routing

--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse, HttpResponseNotFound, Http404
 
 # Create your views here.
+
+
+def january(request: HttpRequest):
+    return HttpResponse(f"Vegan for the entire month")
+
+
+def february(request: HttpRequest):
+    return HttpResponse(f"Taking a photo every day")
+
+
+def march(request: HttpRequest):
+    return HttpResponse(f"100 Pushups every day")
+
+
+def get_challenge(request: HttpRequest, month) -> HttpResponse:
+    response = None
+    if month == "january":
+        response = january(request)
+    elif month == "february":
+        response = february(request)
+    elif month == "march":
+        response = march(request)
+    else:
+        return HttpResponseNotFound(f"Month '{month}' is not supported yet")
+    return response
