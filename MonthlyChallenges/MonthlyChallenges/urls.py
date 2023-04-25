ebbc6155b450eb9382e8e78ddea0874de2d888a9
Simon Alphonse <IAmSimon@outlook.in>
2023-04-25 12:02:29 +0530

    Recreated project

--- MonthlyChallenges/MonthlyChallenges/urls.py
+++ MonthlyChallenges/MonthlyChallenges/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for DjangoApp project.
+URL configuration for MonthlyChallenges project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
