fb55230569cbb706514f5fc307c3a682a88d54e0
Simon Alphonse <IAmSimon@outlook.in>
2023-04-27 11:02:37 +0530

    Added content for welcome page

--- my_site/blog/urls.py
+++ my_site/blog/urls.py
@@ -3,7 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.posts,),
-    path("posts", views.posts),
-    path("posts/<slug:slug>", views.post, name="post_url"),
+    path("", views.welcome),
+    path("welcome/", views.welcome),
+    path("posts/", views.posts),
+    path("posts/<slug:slug>", views.post, name="path_post"),
 ]
--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -4,9 +4,13 @@ from django.shortcuts import render
 # Create your views here.
 
 
+def welcome(request: HttpRequest) -> HttpResponse:
+    return render(request, "blog/welcome.html")
+
+
 def posts(request: HttpRequest) -> HttpResponse:
-    return HttpResponse()
+    return render(request, "blog/posts.html")
 
 
 def post(request: HttpRequest, slug: str) -> HttpResponse:
-    return HttpResponse()
+    return render(request, "blog/post.html")
