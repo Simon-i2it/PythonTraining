219844afcbed006745f792105eaf7ced765436d7
Simon Alphonse <IAmSimon@outlook.in>
2023-04-28 00:28:23 +0530

    Added 404

--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -10,7 +10,7 @@ dict_posts = [
         "title": "Digital Art",
         "image": "digital-art.jpg",
         "author": "Simon Alphonse",
-        "date": date(2023, 4, 26),
+        "date": date(2023, 4, 25),
         "summary": """
             Artists and designers can enhance their digital artwork with a pen drawing tablet.
             This device enables the use of a stylus, providing more control and precision in digital art creation.
@@ -26,7 +26,7 @@ dict_posts = [
         "title": "Table Tennis",
         "image": "table-tennis.jpg",
         "author": "Simon Alphonse",
-        "date": date(2023, 4, 27),
+        "date": date(2023, 4, 26),
         "summary": """
             Table tennis aka ping pong, is the most popular racket sport in the world, with over 300 million active players
             and was played in England in the 1880s instead of tennis and was included in the Olympic Games in 1988.
@@ -38,6 +38,53 @@ dict_posts = [
             - Reduces stress and provides an enjoyable way to relieve tension.
             - Enhances mental acuity, requiring strategic thinking, concentration, and focus.
             - Improves balance and coordination through quick movements and changes in direction.
+        """,
+    },
+    {
+        "slug": "cycling",
+        "title": "Cycling",
+        "image": "cycling.jpg",
+        "author": "Simon Alphonse",
+        "date": date(2023, 4, 27),
+        "summary": """
+            Are you looking to stay healthy while also making a positive impact on the environment? 
+            Look no further than cycling ! As a cyclist, improve your own health & fitness, 
+            and also combat climate change by reducing traffic congestion.
+        """,
+        "content": """
+            - Enhanced cardiovascular health : regular cycling can help to lower blood pressure, reduce the risk of heart disease, and improve overall heart health.
+            - Reduced stress and anxiety : cycling provides a sense of freedom and escape from daily stressors, allowing you to clear your mind and enjoy the outdoors.
+            - Improved overall fitness and endurance : cycling is an excellent form of exercise that engages multiple muscle groups and can help you build endurance and strength over time.
+            - Lower risk of chronic diseases : cycling has been shown to lower the risk of chronic diseases such as diabetes, obesity, and heart disease.
+            - Enhanced mental wellbeing : cycling can help boost your mood and improve mental health, reducing symptoms of anxiety and depression.
+            - Improved joint mobility and flexibility : cycling is a low-impact form of exercise that can help increase joint mobility and flexibility.
+            - Reduced impact on the environment : cycling is an environmentally friendly mode of transportation that produces no carbon emissions and helps reduce traffic congestion.
+            - Increased social connections : cycling provides a great opportunity to connect with like-minded individuals, whether through group rides, clubs, or races.
+            - Improved sleep quality : cycling can help regulate your sleep cycle, leading to better quality sleep and more restful nights.
+            - Cost-effective : cycling is a relatively low-cost activity that requires minimal equipment, making it accessible to a wide range of people.
+        """,
+    },
+    {
+        "slug": "photography",
+        "title": "Photography",
+        "image": "photography.jpg",
+        "author": "Simon Alphonse",
+        "date": date(2023, 4, 28),
+        "summary": """
+            Are you interested in capturing the beauty of the world around you?
+            Look no further than photography! As a photographer, you can explore your creativity and express yourself while capturing moments and memories.
+        """,
+        "content": """
+            - Express creativity : photography allows you to express your creativity and capture unique and beautiful images that showcase your perspective.
+            - Preserve memories : photography is a great way to capture special moments and memories, preserving them for a lifetime.
+            - Develop patience and attention to detail : photography requires patience and attention to detail in order to capture the perfect shot.
+            - Learn about the world : photography can be a tool for exploring and learning about the world around you, including nature, cultures, and people.
+            - Improve technical skills : photography involves learning technical skills such as lighting, composition, and editing, which can improve your overall photography ability.
+            - Develop a unique style : through experimentation and practice, photographers can develop a unique style that sets them apart from others.
+            - Connect with others : photography can be a great way to connect with others who share your passion, whether through online communities or in-person events.
+            - Boost confidence : successfully capturing a great shot can be a confidence booster, improving your self-esteem and sense of accomplishment.
+            - Tell a story : photography can tell a story through a single image, conveying emotions and experiences in a powerful way.
+            - Find beauty in everyday life : photography encourages you to look for the beauty in everyday life, finding inspiration in the world around you.
         """,
     },
 ]
