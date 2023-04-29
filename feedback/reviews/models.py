d2d917af718a792776de413e7e77a77b1c3e4030
Simon Alphonse <IAmSimon@outlook.in>
2023-04-29 20:00:09 +0530

    Added database Review model

--- feedback/reviews/models.py
+++ feedback/reviews/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Review(models.Model):
+    username = models.CharField(max_length=100)
+    feedback = models.CharField(max_length=1000)
+    rating = models.IntegerField()
--- feedback/reviews/views.py
+++ feedback/reviews/views.py
@@ -2,16 +2,25 @@ from django.http import HttpRequest
 from django.shortcuts import redirect, render
 
 from reviews.forms import ReviewForm
+from reviews.models import Review
 
 # Create your views here.
 
 
 def review(request: HttpRequest):
+    form = ReviewForm()
     if request.method == "POST":
         form = ReviewForm(request.POST)
         if form.is_valid():
-            return redirect(thank_you, form.cleaned_data["username"])
-    return render(request, "reviews/review.html", {"form": ReviewForm()})
+            data = form.cleaned_data
+            review = Review(
+                username=data["username"],
+                feedback=data["feedback"],
+                rating=data["rating"],
+            )
+            review.save()
+            return redirect(thank_you, review.username)
+    return render(request, "reviews/review.html", {"form": form})
 
 
 def thank_you(request: HttpRequest, username: str):
