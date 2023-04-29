3caed9be385929f126db5f157934384bbe5eb23c
Simon Alphonse <IAmSimon@outlook.in>
2023-04-29 11:20:30 +0530

    added more fields

--- feedback/reviews/forms.py
+++ feedback/reviews/forms.py
@@ -2,4 +2,13 @@ from django import forms
 
 
 class ReviewForm(forms.Form):
-    username = forms.CharField()
+    username = forms.CharField(
+        label="Name",
+        max_length=100,
+        error_messages={
+            "required": "Name must not be empty",
+            "max_length": "Please enter a shorter name",
+        },
+    )
+    feedback = forms.CharField(widget=forms.Textarea, max_length=2000)
+    rating = forms.IntegerField(min_value=1, max_value=5)
--- feedback/reviews/views.py
+++ feedback/reviews/views.py
@@ -1,6 +1,5 @@
 from django.http import HttpRequest
 from django.shortcuts import redirect, render
-from django.urls import reverse
 
 from reviews.forms import ReviewForm
 
@@ -8,13 +7,11 @@ from reviews.forms import ReviewForm
 
 
 def review(request: HttpRequest):
-    form: ReviewForm = ReviewForm()
     if request.method == "POST":
-        submitted_form = ReviewForm(request.POST)
-        if submitted_form.is_valid():
-            return redirect(thank_you, submitted_form.cleaned_data["username"])
-    else:
-        return render(request, "reviews/review.html", {"form": form})
+        form = ReviewForm(request.POST)
+        if form.is_valid():
+            return redirect(thank_you, form.cleaned_data["username"])
+    return render(request, "reviews/review.html", {"form": ReviewForm()})
 
 
 def thank_you(request: HttpRequest, username: str):
