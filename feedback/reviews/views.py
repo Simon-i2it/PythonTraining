4e96b9705560972a479684023f3249c903226a48
Simon Alphonse <IAmSimon@outlook.in>
2023-04-30 13:09:23 +0530

    Added CreateView

--- feedback/reviews/views.py
+++ feedback/reviews/views.py
@@ -6,26 +6,33 @@ from django.urls import reverse
 from django.views.generic.base import TemplateView
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import FormView
+from django.views.generic.edit import CreateView
 
 
-from reviews.forms import ReviewForm
+# from reviews.forms import ReviewForm
 from reviews.models import Review
 
 # Create your views here.
 
 
-class ReviewView(FormView):
-    form_class = ReviewForm
+class ReviewView(CreateView):
+    model = Review
+    fields = "__all__"
+    # exclude=["rating"]
+    labels = {
+        "username": "Name",
+    }
+    error_messages = {
+        "username": {
+            "required": "Name must not be empty",
+            "max_length": "Please enter a shorter name",
+        }
+    }
     template_name = "reviews/review.html"
     success_url = "/thank_you"
 
     def get_success_url(self) -> str:
         return f"{super().get_success_url()}/{self.request.POST.get('username')}"
-
-    def form_valid(self, form: ReviewForm) -> HttpResponse:
-        form.save()
-        return super().form_valid(form)
 
 
 class ThankYouView(TemplateView):
