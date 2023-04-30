d0555eaf6feca6006d09f2b80a7ac54106a8c9c8
Simon Alphonse <IAmSimon@outlook.in>
2023-04-30 10:42:00 +0530

    Added TemplateView, ListView, DetailView

--- feedback/reviews/urls.py
+++ feedback/reviews/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     path("", views.ReviewView.as_view()),
+    path("reviews", views.ReviewsView.as_view()),
+    path("review/<int:pk>", views.ReviewDetailView.as_view(), name="url-review-detail"),
     path(
         "thank_you/<str:username>", views.ThankYouView.as_view(), name="url-thank-you"
     ),
--- feedback/reviews/views.py
+++ feedback/reviews/views.py
@@ -1,9 +1,13 @@
 from typing import Any, Dict
+from django.db.models.query import QuerySet
 from django.http import HttpRequest
 from django.shortcuts import redirect, render
 from django.views.generic.base import TemplateView
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 
 from reviews.forms import ReviewForm
+from reviews.models import Review
 
 # Create your views here.
 
@@ -22,3 +26,17 @@ class ReviewView(TemplateView):
 
 class ThankYouView(TemplateView):
     template_name = "reviews/thank_you.html"
+
+
+class ReviewsView(ListView):
+    model = Review
+    context_object_name = "reviews"
+    template_name = "reviews/reviews.html"
+
+    def get_queryset(self) -> QuerySet[Any]:
+        return super().get_queryset().filter(rating__lte=2)
+
+
+class ReviewDetailView(DetailView):
+    model = Review
+    template_name = "reviews/review_detail.html"
