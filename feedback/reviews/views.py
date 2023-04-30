1e8cd64183115e877413dd7715449e74cf95897d
Simon Alphonse <IAmSimon@outlook.in>
2023-04-30 09:13:36 +0530

    Fixed update logic

--- feedback/reviews/views.py
+++ feedback/reviews/views.py
@@ -10,19 +10,14 @@ from reviews.models import Review
 def review(request: HttpRequest):
     form = ReviewForm()
     if request.method == "POST":
-        username = form.cleaned_data["username"]
-        instance = Review.objects.get(username=username)
-
-        if instance is not None:
-            form = ReviewForm(request.POST, instance=instance)
-        else:
-            form = ReviewForm(request.POST)
+        form = ReviewForm(request.POST)
+        # form = ReviewForm(request.POST, Review.objects.get(username="username")) # to update record
 
         if form.is_valid():
+            username = form.cleaned_data["username"]
             form.save()
-            return redirect(
-                thank_you,
-            )
+            return redirect(thank_you, username)
+
     return render(request, "reviews/review.html", {"form": form})
 
 
