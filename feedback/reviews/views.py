24215ea4d259787733035623eb3ecf30e87b0ac7
Simon Alphonse <IAmSimon@outlook.in>
2023-04-30 10:00:39 +0530

    Added TemplateView

--- feedback/reviews/views.py
+++ feedback/reviews/views.py
@@ -1,26 +1,24 @@
+from typing import Any, Dict
 from django.http import HttpRequest
 from django.shortcuts import redirect, render
-from django.views import View
+from django.views.generic.base import TemplateView
 
 from reviews.forms import ReviewForm
-from reviews.models import Review
 
 # Create your views here.
 
 
-class ReviewView(View):
+class ReviewView(TemplateView):
     def get(self, request: HttpRequest):
         form = ReviewForm()
         return render(request, "reviews/review.html", {"form": form})
 
     def post(self, request: HttpRequest):
         form = ReviewForm(request.POST)
-        # form = ReviewForm(request.POST, Review.objects.get(username="username")) # to update record
         if form.is_valid():
             form.save()
             return redirect("url-thank-you", form.cleaned_data["username"])
 
 
-class ThankYouView(View):
-    def get(self, request: HttpRequest, username: str):
-        return render(request, "reviews/thank_you.html", {"username": username})
+class ThankYouView(TemplateView):
+    template_name = "reviews/thank_you.html"
