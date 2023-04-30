e1e007941797f5535922469c7290c33bfb603cd6
Simon Alphonse <IAmSimon@outlook.in>
2023-04-30 08:56:03 +0530

    Added update feedback logic to ModelForm

--- feedback/reviews/views.py
+++ feedback/reviews/views.py
@@ -2,6 +2,7 @@ from django.http import HttpRequest
 from django.shortcuts import redirect, render
 
 from reviews.forms import ReviewForm
+from reviews.models import Review
 
 # Create your views here.
 
@@ -9,10 +10,19 @@ from reviews.forms import ReviewForm
 def review(request: HttpRequest):
     form = ReviewForm()
     if request.method == "POST":
-        form = ReviewForm(request.POST)
+        username = form.cleaned_data["username"]
+        instance = Review.objects.get(username=username)
+
+        if instance is not None:
+            form = ReviewForm(request.POST, instance=instance)
+        else:
+            form = ReviewForm(request.POST)
+
         if form.is_valid():
             form.save()
-            return redirect(thank_you, form.cleaned_data["username"])
+            return redirect(
+                thank_you,
+            )
     return render(request, "reviews/review.html", {"form": form})
 
 
