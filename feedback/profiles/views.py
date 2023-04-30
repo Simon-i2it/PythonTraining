8aa5364fcd8100340c296b108f969beccfeac643
Simon Alphonse <IAmSimon@outlook.in>
2023-04-30 17:46:49 +0530

    Added ImageField in Form and Model

--- feedback/profiles/views.py
+++ feedback/profiles/views.py
@@ -1,18 +1,21 @@
 from django.http import HttpRequest
 from django.shortcuts import redirect, render
-from django.urls import reverse
 from django.views import View
+
+from profiles.forms import ProfileForm
+from profiles.models import Profile
 
 # Create your views here.
 
 
 class ProfileView(View):
     def get(self, request):
-        return render(request, "profiles/profile.html")
+        form = ProfileForm()
+        return render(request, "profiles/profile.html", {"form": form})
 
     def post(self, request: HttpRequest):
-        print(request.FILES["image"])
-        with open("profiles/temp/image.jpg", "wb+") as destination:
-            for chunk in request.FILES["image"].chunks():
-                destination.write(chunk)
-        return redirect("/profile")
+        submitted_form = ProfileForm(request.POST, request.FILES)
+        if submitted_form.is_valid():
+            profile = Profile(image_file=request.FILES["image_file"])
+            profile.save()
+        return render(request, "profiles/profile.html", {"form": submitted_form})
